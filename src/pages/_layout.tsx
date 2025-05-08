a022f7c11d4ea46b04f69e31a21d8ea3076f1387
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-05-08 13:21:01 +0200

    feat: initial setup of hero

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -2,23 +2,17 @@ import '../styles.css';
 
 import type { ReactNode } from 'react';
 
-import { Header } from '../components/header';
-import { Footer } from '../components/footer';
-
 type RootLayoutProps = { children: ReactNode };
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   const data = await getData();
 
   return (
-    <div className="font-['Nunito']">
+    <div className="h-full">
+      <title>hyperlink-drifter</title>
       <meta name="description" content={data.description} />
       <link rel="icon" type="image/png" href={data.icon} />
-      <Header />
-      <main className="m-6 flex items-center *:min-h-64 *:min-w-64 lg:m-0 lg:min-h-svh lg:justify-center">
-        {children}
-      </main>
-      <Footer />
+      <main className="h-full grid grid-cols-1 grid-rows-1">{children}</main>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,32 @@
-import { Link } from 'waku';
-
-import { Counter } from '../components/counter';
-
 export default async function HomePage() {
-  const data = await getData();
-
   return (
-    <div>
-      <title>{data.title}</title>
-      <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
-      <p>{data.body}</p>
-      <Counter />
-      <Link to="/about" className="mt-4 inline-block underline">
-        About page
-      </Link>
-    </div>
+    <>
+      <picture className="grid grid-cols-subgrid grid-rows-subgrid col-span-full row-span-full place-self-stretch">
+        <source
+          type="image/webp"
+          srcSet="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2ZqbWs2aWIxcGx2MTFwMTY1aWt6ZmtwdmlxYWI2cW0zYWxpa2JlcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7qE6GmiEj9QsrAKA/giphy.webp"
+        />
+        <img
+          className="col-span-full row-span-full place-self-stretch object-cover"
+          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2ZqbWs2aWIxcGx2MTFwMTY1aWt6ZmtwdmlxYWI2cW0zYWxpa2JlcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7qE6GmiEj9QsrAKA/giphy.gif"
+          width="100%"
+          height="100%"
+          alt="80s smile GIF by eniac"
+          style={{ background: 'unset' }}
+        />
+      </picture>
+      <div className="grid grid-cols-2 grid-rows-[1fr_33svh] col-span-full row-span-full place-self-stretch">
+        <h1 className="grid grid-cols-subgrid grid-rows-subgrid col-span-full row-span-full w-max text-8xl font-black tracking-wider text-transparent bg-linear-[25deg,#fff103_30%,#e709f1] bg-clip-text writing-mode-v-rl">
+          <span className="col-span-full">hyperlink</span>
+          <span className="col-start-1">drifter</span>
+        </h1>
+        <small className="col-start-1 row-start-2 place-self-end text-sm text-[#fff103] font-normal">
+          More to come
+        </small>
+      </div>
+    </>
   );
 }
-
-const getData = async () => {
-  const data = {
-    title: 'Waku',
-    headline: 'Waku',
-    body: 'Hello world!',
-  };
-
-  return data;
-};
 
 export const getConfig = async () => {
   return {
